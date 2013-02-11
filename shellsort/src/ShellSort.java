cdb50db8ab48bcbd4d9a70217d9a84459e14fb38
Akhilesh Nayak <nayakhilesh@gmail.com>
2013-02-10 22:42:08 -0500

    changed return types to void

--- shellsort/src/ShellSort.java
+++ shellsort/src/ShellSort.java
@@ -1,10 +1,10 @@
 
 public class ShellSort {
  	
-	public static int[] shell(int n, int[] arr) {
+	public static void shell(int n, int[] arr) {
 		
 		if (n <= 0 || n > arr.length) {
-			return arr;
+			return;
 		}
 		
 		for (int i=0;i<=(n-1);i++) {
@@ -22,19 +22,17 @@ public class ShellSort {
 			}
 			
 		}
-		
-		return arr;
+
 	}
  
-	public static int[] shellSort(int[] arr) {
+	public static void sort(int[] arr) {
 		
 		int n = (arr.length/2);
 		if(n % 2 == 0) {
 			n-=1;
 		}
 		for(int i=n;i>=1;i-=2) {
-			arr = shell(i,arr);
+			shell(i,arr);
 		}
-		return arr;
 	}
  }
--- shellsort/src/ShellSortTest.java
+++ shellsort/src/ShellSortTest.java
@@ -15,13 +15,13 @@ public class ShellSortTest {
 	public void testShell() {
 		
 		int[] arr = {5,7,3,8,2,6,1,4,9,0};
-		arr = ShellSort.shell(4, arr);
+		ShellSort.shell(4, arr);
 		assertTrue("[2, 0, 1, 4, 5, 6, 3, 8, 9, 7]".equals(Arrays.toString(arr)));
 		
-		arr = ShellSort.shell(2, arr); 
+		ShellSort.shell(2, arr); 
 		assertTrue("[1, 0, 2, 4, 3, 6, 5, 7, 9, 8]".equals(Arrays.toString(arr)));
 		
-		arr = ShellSort.shell(1, arr);
+		ShellSort.shell(1, arr);
 		assertTrue("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]".equals(Arrays.toString(arr)));
 		
 	}
@@ -30,7 +30,7 @@ public class ShellSortTest {
 	public void testShellSort() {
 		
 		int[] arr = {5,7,3,8,2,6,1,4,9,0};
-		arr = ShellSort.shellSort(arr);
+		ShellSort.sort(arr);
 		assertTrue("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]".equals(Arrays.toString(arr)));
 	
 	}
